2fa81cc6edbb22c5d858ab769935d20b7f1478b9
Warths <pro.warths@gmail.com>
2018-03-10 16:31:31 +0100

    Full rewrite of the code
    Almost entirely rewrite the code to avoid creating conflict between json data and loaded data ingame.
    Now only interacts with commands sent to the console. 
    Does not need to requests all UUID at once now and just requests one at a time to verify that the account actually exist.

--- start.py
+++ start.py
@@ -1,43 +1,40 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding:utf-8
 
 import json
 import requests
 import subprocess
-import time
 
 PROFILES_URL = "https://api.mojang.com/profiles/minecraft"
+# Twitch Partners and Affiliates can use whitelist.twitchapps.com json export
+# You can use something else as long as json keys are respected
 STREAM_WHITELIST_URL = "https://whitelist.twitchapps.com/list.php?id=warths5657829972d9b&format=json"
+# Relative path to Whitelist and OP files
 WHITELIST_FILE = "whitelist.json"
 OPS_FILE = "ops.json"
-ALL_OPS = True  # If True, Every whitelisted player will be OP. May be useful for Creative Servers.
-OPS_PERMISSION_LEVEL = 2 # AS
-OPS_BYPASSES_PLAYER_LIMIT = False
-LEVEL_4_OP_NAME = ["Warths", "Guerrierra", "Everynight_MC"]
-EXCLUDED_OPS = ["MyTheValentinus"]
-RELOAD_OP_LIST = True  # Force a Server Restart if OP list is changed
-SCREEN_NAME = "minecraft"
+# If True, all players will be OP on the server, with the Userlevel specified in server.properties
+GLOBAL_OP_PERMISSIONS = True
+EXCLUDED_OPS = []
+EXCLUDED_OPS = [name.lower() for name in EXCLUDED_OPS]
+# Op that can't be altered by the program.
+PRECONFIGURED_OP = ["Warths", "Guerrierra", "Everynight_MC", "Verrhues"]
+PRECONFIGURED_OP = [name.lower() for name in PRECONFIGURED_OP]
+SCREEN_NAME = "server"
+# Will log every changes in the Minecraft Chat if True
+TELLRAW_LOG = True
+
 
 
 def _post(url, body, head):
     response = requests.post(url, data=body, headers=head)
     return response.json()
 
-
-def find_profiles_by_names(subwl_names):
+def GetUUID(name):
     result = []
-    pages = [subwl_names[i:i + 100] for i in range(0, len(subwl_names), 100)]
-    for page in pages:
-        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        body = json.dumps(page)
-        result += _post(PROFILES_URL, body, headers)
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    body = json.dumps(name)
+    result = _post(PROFILES_URL, body, headers)
     return result
-
-
-def format_uuid(uuid):
-    formatted_uuid = "%s-%s-%s-%s-%s" % (uuid[:8], uuid[8:12], uuid[12:16], uuid[16:20], uuid[20:])
-    return formatted_uuid
-
 
 def run_cmd(cmd):
     try:
@@ -47,16 +44,45 @@ def run_cmd(cmd):
         return
 
 
-def stop_server():
-    for t in range(15, 0, -5):
-        run_minecraft_cmd('say Stopping in %s ...' % str(t))
-        time.sleep(5)
-    run_minecraft_cmd('stop')
-
-
 def run_minecraft_cmd(string):
     string = string.replace('"', '\\"')
     run_cmd('screen -S %s -X stuff "%s^M"' % (SCREEN_NAME, string))
+
+
+def action(action, player_name, reason="Unspecified"):
+    if not GetUUID(player_name):
+        print("UUID doesn't refer to existing account, skipping %s" % player_name)
+        return
+
+    if action == "whitelist":
+        command_strings = ["whitelist add %s" % player_name, "Ajout de %s dans la Whitelist" % player_name]
+    elif action == "unwhitelist":
+        command_strings = ["whitelist remove %s" % player_name, "Retrait de %s de la Whitelist" % player_name]
+        run_minecraft_cmd("kick %s Le joueur n'est pas dans la whitelist" % player_name)
+    elif action == "op":
+        command_strings = ["op %s" % player_name, "Ajout de %s dans le groupe Operators" % player_name]
+    elif action == "deop":
+        command_strings = ["deop %s" % player_name, "Retrait de %s du groupe Operators" % player_name]
+    else:
+        log('tellraw @a {"text":"Commande non gérée( %s ).'
+            + 'Abandon","italic":true,"color":"red"}' % action)
+        return
+    run_minecraft_cmd(command_strings[0])
+    log('tellraw @a {"text":"%s (Raison : %s)","italic":true,"color":"gray"}' % (command_strings[1], reason))
+
+
+def log(log_content):
+        if TELLRAW_LOG:
+            run_minecraft_cmd(log_content)
+
+
+def mc_json_to_list(file):
+    file_list = []
+    with open(file, "r") as loadedfile:
+        file_list_json = json.load(loadedfile)
+    for name in file_list_json:
+        file_list.append(name['name'].lower())
+    return file_list
 
 
 if __name__ == '__main__':
@@ -65,41 +91,34 @@ if __name__ == '__main__':
     request = requests.get(STREAM_WHITELIST_URL, headers=headers)
     if request.status_code == 200:
         result = request.json()
-        names = []
+
+        old_whitelist = mc_json_to_list(WHITELIST_FILE)
+        new_whitelist = []
         for name in result:
-            names.append(result[name]['whitelist_name'])
-        list_player = find_profiles_by_names(names)
-        whitelist = []
-        for player in list_player:
-            full_uuid = format_uuid(player['id'])
-            whitelist_block = {'uuid': full_uuid, 'name': player['name']}
-            whitelist.append(whitelist_block)
-        whitelist = json.dumps(whitelist, indent=4)
-        try:
-            with open(WHITELIST_FILE, 'w') as Whitelist:
-                Whitelist.write(whitelist)
-                Whitelist.close()
-        except:
-            print("Error when writing whitelist file")
-        if ALL_OPS:
-            with open(OPS_FILE, 'r') as file:
-                ops_json = json.load(file)
-            with open(WHITELIST_FILE, 'r') as file:
-                whitelist_json = json.load(file)
-            for op_player in ops_json:
-                if op_player['uuid'] in whitelist:
-                    if op_player['name'] in EXCLUDED_OPS:
-                        run_minecraft_cmd("deop %s" % (op_player['name']))
-                else:
-                    run_minecraft_cmd("op %s" % (op_player['name']))
-            ops_json = json.dumps(ops_json, indent=4)
-            for whitelisted_player in whitelist_json:
-                if whitelisted_player['uuid'] in ops_json:
-                    if whitelisted_player['name'] in EXCLUDED_OPS:
-                        run_minecraft_cmd("deop %s" % (whitelisted_player['name']))
-                elif whitelisted_player['name'] not in EXCLUDED_OPS:
-                        run_minecraft_cmd("op %s" % (whitelisted_player['name']))
+            new_whitelist.append(result[name]['whitelist_name'].lower())
 
+        for player_name in new_whitelist: # Adding new players to whitelist
+            if player_name not in old_whitelist:
+                action("whitelist", player_name.capitalize(), "Ajout dans la SubWhitelist")
 
+        for player_name in old_whitelist: # Removing old players from new whitelist
+            if player_name not in new_whitelist:
+                action("unwhitelist", player_name.capitalize(), "Le joueur n'est pas dans la SubWhitelist")
 
+        old_oplist = mc_json_to_list(OPS_FILE)
+        if GLOBAL_OP_PERMISSIONS:
+            for player_name in new_whitelist:
+                if player_name not in old_oplist \
+                        and player_name not in EXCLUDED_OPS \
+                        and player_name not in PRECONFIGURED_OP:
+                    action("op", player_name.capitalize(), "Droits OP globaux")
+                elif player_name in old_oplist and player_name in EXCLUDED_OPS:
+                    action("deop", player_name.capitalize(), "Joueur exclu des permissions")
 
+            for player_name in old_oplist:
+                if player_name not in new_whitelist and player_name not in PRECONFIGURED_OP:
+                    action("deop", player_name.capitalize(), "Le joueur n'est pas dans la Whitelist.")
+        else:
+            for player_name in old_oplist:
+                if player_name not in PRECONFIGURED_OP:
+                    action("deop", player_name.capitalize(), "Permissions globales inactives")
